b59ba4aa42a72e5f8a0146e0bd81105b81f55166
fanne <896661380@qq.com>
2017-07-16 15:09:55 +0800

    7.6,完成课程页面筛选功能

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -13,6 +13,25 @@ class OrgView(View):
     def get(self, request):
         all_orgs = CourseOrg.objects.all()
         all_citys = CityDict.objects.all()
+        hot_orgs = all_orgs.order_by("-click_nums")[:3]
+
+        # 取出筛选城市
+        city_id = request.GET.get("city", "")
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 类别筛选
+        catgory = request.GET.get("ct", "")
+        if catgory:
+            all_orgs = all_orgs.filter(catgory=catgory)
+
+        sort = request.GET.get("sort", "")
+        if sort:
+            if sort == "students":
+                all_orgs = all_orgs.order_by("-students")
+            elif sort == "courses":
+                all_orgs = all_orgs.order_by("-course_nums")
+
         org_nums = all_orgs.count()
 
         # 对课程机构进行分页
@@ -28,4 +47,8 @@ class OrgView(View):
         return render(request, "org-list.html",
                       {"all_orgs": orgs,
                        "all_citys": all_citys,
-                       "org_nums": org_nums})
+                       "org_nums": org_nums,
+                       "city_id": city_id,
+                       "catgory": catgory,
+                       "hot_orgs": hot_orgs,
+                       "sort": sort})
