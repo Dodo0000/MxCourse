030c372e025276914cd93f7b7c2e3d1e8b678f01
fanne <896661380@qq.com>
2017-07-17 22:55:48 +0800

    7.11,机构详情页面内容

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -36,6 +36,9 @@ class CourseOrg(models.Model):
         verbose_name = u"课程机构"
         verbose_name_plural = verbose_name
 
+    def __unicode__(self):
+        return self.name
+
 class Teacher(models.Model):
     org = models.ForeignKey(CourseOrg, verbose_name=u"所属机构")
     name = models.CharField(max_length=50, verbose_name=u"教师名")
@@ -43,6 +46,7 @@ class Teacher(models.Model):
     work_company = models.CharField(max_length=50, verbose_name=u"就职公司")
     work_position = models.CharField(max_length=50, verbose_name=u"公司职位")
     points = models.CharField(max_length=50, verbose_name=u"教学特点")
+    image = models.ImageField(max_length=100, upload_to='teacher/%Y%m', verbose_name=u"头像", default="")
     click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
     fav_nums = models.IntegerField(default=0, verbose_name=u"收藏数")
     add_time = models.DateTimeField(default=datetime.now)
