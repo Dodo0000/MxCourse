2f25f8539348c156039b2a706089f51a1889ea90
fanne <896661380@qq.com>
2017-07-19 15:42:35 +0800

    7.12,用户收藏

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .models import CourseOrg,CityDict
+from operation.models import UserFavorite
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from forms import UserAskForm
@@ -75,6 +76,10 @@ class OrgHomeView(View):
     def get(self, request, org_id):
         current_page = "home"
         course_org = CourseOrg.objects.get(id=int(org_id))
+        has_fav = False
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+                has_fav = True
         all_courses = course_org.course_set.all()[:3]
         # 有外键地方都可以这么取值
         all_teachers = course_org.teacher_set.all()[:1]
@@ -83,6 +88,7 @@ class OrgHomeView(View):
             "all_teachers": all_teachers,
             "course_org": course_org,
             "current_page": current_page,
+            "has_fav": has_fav,
         })
 
 
@@ -93,12 +99,17 @@ class OrgCourseView(View):
     def get(self, request, org_id):
         current_page = "course"
         course_org = CourseOrg.objects.get(id=int(org_id))
+        has_fav = False
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+                has_fav = True
         all_courses = course_org.course_set.all()
         # 有外键地方都可以这么取值
         return render(request, 'org-detail-course.html', {
             "all_courses": all_courses,
             "course_org": course_org,
             "current_page": current_page,
+            "has_fav": has_fav,
         })
 
 
@@ -109,10 +120,15 @@ class OrgDescView(View):
     def get(self, request, org_id):
         current_page = "desc"
         course_org = CourseOrg.objects.get(id=int(org_id))
+        has_fav = False
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+                has_fav = True
         # 有外键地方都可以这么取值
         return render(request, 'org-detail-desc.html', {
             "course_org": course_org,
             "current_page": current_page,
+            "has_fav": has_fav,
         })
 
 
@@ -124,9 +140,48 @@ class OrgTeacherView(View):
         current_page = "teacher"
         course_org = CourseOrg.objects.get(id=int(org_id))
         all_teachers = course_org.teacher_set.all()
+        has_fav = False
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+                has_fav = True
         # 有外键地方都可以这么取值
         return render(request, 'org-detail-teachers.html', {
             "course_org": course_org,
             "all_teachers": all_teachers,
             "current_page": current_page,
+            "has_fav": has_fav,
         })
+
+class AddFavView(View):
+    """
+    用户收藏
+    """
+    def post(self,request):
+        fav_id = request.POST.get("fav_id", 0)
+        fav_type = request.POST.get("fav_type", 0)
+
+        if not request.user.is_authenticated():
+            """
+            此处user为一个匿名类，django内置的一种方法，此user与正常的user有相似的用法
+            所以此处调用user.is_authenticated()方法，后面带括号.
+            """
+            return HttpResponse('{"status": "fail", "msg":"用户未登录"}', content_type="application/json")
+
+        exit_recods = UserFavorite.objects.filter(user=request.user, fav_id=int(fav_id), fav_type=int(fav_type))
+        if exit_recods:
+            """
+            记录已经存在，则表示要取消收藏
+            """
+            exit_recods.delete()
+            return HttpResponse('{"status": "success", "msg":"收藏"}', content_type="application/json")
+        else:
+            user_fav = UserFavorite()
+            if int(fav_id) > 0 and int(fav_type) > 0:
+                user_fav.user = request.user
+                user_fav.fav_id = int(fav_id)
+                user_fav.fav_type = int(fav_type)
+                user_fav.save()
+                return HttpResponse('{"status": "success", "msg":"已收藏"}', content_type="application/json")
+            else:
+                return HttpResponse('{"status": "fail", "msg":"收藏出错"}', content_type="application/json")
+
