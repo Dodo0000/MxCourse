8476c4e36e7c7658fa170d10deb8aafc4b56a644
fanne <896661380@qq.com>
2017-07-28 18:30:42 +0800

    10.5,修改用户密码

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -6,6 +6,7 @@ __Date__ = '2017/7/10'
 
 from django import forms
 from captcha.fields import CaptchaField
+from .models import UserProfile
 
 
 class LoginForm(forms.Form):
@@ -27,3 +28,8 @@ class ForgetForm(forms.Form):
 class ModifyPwdForm(forms.Form):
     password1 = forms.CharField(required=True, min_length=5)
     password2 = forms.CharField(required=True, min_length=5)
+
+class UploadImageForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ["image"]
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,12 +4,14 @@ __Author__ = 'eyu Fanne'
 __Date__ = '2017/7/27'
 
 from django.conf.urls import  url, include
-from .views import UserInfoView
+from .views import UserInfoView, UploadImageView, UpdatePwdView
 
 
 
 urlpatterns = [
     # 用户信息
     url(r'^info/$', UserInfoView.as_view(), name="user_info"),
+    url(r'^image/upload/$', UploadImageView.as_view(), name="image_upload"),
+    url(r'^update/pwd/$', UpdatePwdView.as_view(), name="update_pwd"),
 
 ]
